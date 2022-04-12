12ae5d134a1a5c790957562d00e6d1cab5d4bb1a
s-m-quadri <quadrisyedminnatullah@gmail.com>
2022-04-12 20:11:08 +0530

    small indentation fix

--- DIEMS_Labs/OS/01_File_Handling.cpp
+++ DIEMS_Labs/OS/01_File_Handling.cpp
@@ -6,46 +6,46 @@
 int main(void){
     //////////////////////////////////////////////////////////////////
     // 1. Write into the file
-	//      a. Open the file to write and check for error
-	//      b. Get the input from user
-	//      c. Put it into the file
-	//      d. Close the file
-	//////////////////////////////////////////////////////////////////
-	FILE *ptr = fopen("abc.txt", "w");
-	if(ptr == NULL){
-		printf("Sorry! somthing went wrong.\n");
-		return 1;
-	}
-	char input[50];
-	printf("Input : ");
-	scanf(" %[^\n]", input);
-	fputs(input, ptr);
-	fclose(ptr);
-	ptr = NULL;
+    //      a. Open the file to write and check for error
+    //      b. Get the input from user
+    //      c. Put it into the file
+    //      d. Close the file
+    //////////////////////////////////////////////////////////////////
+    FILE *ptr = fopen("abc.txt", "w");
+    if(ptr == NULL){
+        printf("Sorry! somthing went wrong.\n");
+        return 1;
+    }
+    char input[50];
+    printf("Input : ");
+    scanf(" %[^\n]", input);
+    fputs(input, ptr);
+    fclose(ptr);
+    ptr = NULL;
 
     //////////////////////////////////////////////////////////////////
     // 2. Read from the file
-	//     a. Open the file to read and check for error
-	//     b. Print the output from the file
-	//     c. Close the file
-	//////////////////////////////////////////////////////////////////
-	ptr = fopen("abc.txt", "r");
-	if(ptr == NULL){
-		printf("Sorry! somthing went wrong.\n");
-		return 1;
-	}
-	printf("Output : ");
-	char buffer = fgetc(ptr);
-	while(buffer != EOF){
-		printf("%c", buffer);
-		buffer = fgetc(ptr);
-	}
-	printf("\n");
-	fclose(ptr);
-	ptr = NULL;
+    //     a. Open the file to read and check for error
+    //     b. Print the output from the file
+    //     c. Close the file
+    //////////////////////////////////////////////////////////////////
+    ptr = fopen("abc.txt", "r");
+    if(ptr == NULL){
+        printf("Sorry! somthing went wrong.\n");
+        return 1;
+    }
+    printf("Output : ");
+    char buffer = fgetc(ptr);
+    while(buffer != EOF){
+        printf("%c", buffer);
+        buffer = fgetc(ptr);
+    }
+    printf("\n");
+    fclose(ptr);
+    ptr = NULL;
 
     //////////////////////////////////////////////////////////////////
-	// 3. Done!
-	//////////////////////////////////////////////////////////////////
-	return 0;
+    // 3. Done!
+    //////////////////////////////////////////////////////////////////
+    return 0;
 }
